26fd0cdadbff26027ae258f6b714f21bdb5634b4
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-17 19:58:56 -0400

    Make domino bases

--- javascripts/01_gameSetUp.js
+++ javascripts/01_gameSetUp.js
@@ -1,6 +1,8 @@
-var domino_set
+var domino_set, box
 
 domino_set = [];
+
+set = document.getElementById("set");
 
 
 (function generateDominos () {
@@ -11,6 +13,37 @@ domino_set = [];
     }
   }
 
-  console.log(domino_set);
+  domino_set.forEach(makeDomino);
 
 })()
+
+
+function makeDomino(item) {
+  // create container
+  domino_container = document.createElement("DIV");
+  domino_container.classList.add("domino__container");
+
+  // create domino frame
+  domino_body = document.createElement("DIV");
+  domino_body.classList.add("domino-body");
+  domino_container.appendChild(domino_body);
+
+  // create halves
+  for(var i = 1; i <= 2; i++){
+    domino_half = document.createElement("DIV");
+    domino_half.classList.add("domino-half");
+
+    if (i % 2 == 1) {
+      domino_half.classList.add("domino-body-top");
+    } else {
+      domino_half.classList.add("domino-body-bottom");
+    }
+
+    domino_body.appendChild(domino_half);
+  }
+
+
+
+  // make domino
+  document.getElementById("box_of_dominos").appendChild(domino_container);
+}
