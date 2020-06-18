e4105dba8d4f931e41bcade0443a2cf52a09114e
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-17 21:47:23 -0400

    Domino progress

--- javascripts/01_gameSetUp.js
+++ javascripts/01_gameSetUp.js
@@ -1,24 +1,32 @@
-var domino_set, box
+var domino_set, box, top_dots, top_bottom
 
 domino_set = [];
 
 set = document.getElementById("set");
 
 
-(function generateDominos () {
+(function gameSetUp () {
 
+  generateDominoSet();
+
+})();
+
+function generateDominoSet() {
   for (var y = 0; y <= 9; y++) {
     for (var x = 0; x <= y; x++) {
       domino_set.push({"top": x, "bottom": y});
     }
   }
 
-  domino_set.forEach(makeDomino);
+  domino_set.forEach(buildDominos);
+};
 
-})()
 
+function buildDominos(item, index) {
+  // variables
+  top_dots = item.top;
+  bottom_dots = item.bottom;
 
-function makeDomino(item) {
   // create container
   domino_container = document.createElement("DIV");
   domino_container.classList.add("domino__container");
@@ -26,24 +34,30 @@ function makeDomino(item) {
   // create domino frame
   domino_body = document.createElement("DIV");
   domino_body.classList.add("domino-body");
+  domino_body.classList.add("domino-" + index);
   domino_container.appendChild(domino_body);
 
   // create halves
-  for(var i = 1; i <= 2; i++){
+  for(var i = 1; i < 3; i++){
     domino_half = document.createElement("DIV");
     domino_half.classList.add("domino-half");
 
+    dot_container = document.createElement("DIV");
+    dot_container.classList.add("domino-dot__grid");
+    domino_half.appendChild(dot_container);
+
     if (i % 2 == 1) {
       domino_half.classList.add("domino-body-top");
+      dot_container.classList.add("dot-count-" + top_dots);
     } else {
+      console.log("butt")
       domino_half.classList.add("domino-body-bottom");
+      dot_container.classList.add("dot-count-" + bottom_dots);
     }
 
     domino_body.appendChild(domino_half);
   }
 
-
-
   // make domino
   document.getElementById("box_of_dominos").appendChild(domino_container);
-}
+};
